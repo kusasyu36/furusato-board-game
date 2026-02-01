8ec3b7e1a3f7f8fa1981167d667f498aee781d0c
Shuichi Kusaba <s-kusaba@extrafuture-tech.com>
2026-02-01 11:26:24 +0900

    auto: 11:26:24

---
+++ src/components/game/index.ts
@@ -0,0 +1,7 @@
+export { GameBoard } from './GameBoard';
+export { HappinessGauge } from './HappinessGauge';
+export { PlayerStatus } from './PlayerStatus';
+export { PopulationMeter } from './PopulationMeter';
+export { ActionPanel } from './ActionPanel';
+export { GameLog } from './GameLog';
+export { GameHeader } from './GameHeader';
